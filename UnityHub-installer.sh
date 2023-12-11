d2bb2fbbc1ebfbd1334e9e408ddc356789935615
Andreas Karageorgos <akarageorgos7@protonmail.com>
2023-12-11 12:47:12 +0200

    commit
    Signed-off-by: Andreas Karageorgos <akarageorgos7@protonmail.com>

--- UnityHub-installer.sh
+++ UnityHub-installer.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 apt update
 apt install wget -y
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
@@ -9,4 +14,4 @@ rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 apt update
-apt install unityhub
+apt install unityhub -y
