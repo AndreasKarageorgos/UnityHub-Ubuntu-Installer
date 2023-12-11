97dab4b6f005cb696874608faaf165419096732b
Andreas Karageorgos <akarageorgos7@protonmail.com>
2023-12-11 14:11:00 +0200

    commit
    Signed-off-by: Andreas Karageorgos <akarageorgos7@protonmail.com>

--- UnityHub-installer.sh
+++ UnityHub-installer.sh
@@ -5,16 +5,16 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-apt update
-apt install wget -y
+sudo apt update
+sudo apt install wget -y
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
-apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
-rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
+sudo apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
+sudo rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 
-wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-apt update
-apt install unityhub -y
+sudo apt update
+sudo apt install unityhub -y
 
 clear
 
