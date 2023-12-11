4108a47436252ce11cca9f5ddb0c2a5b807782a5
Andreas Karageorgos <akarageorgos7@protonmail.com>
2023-12-11 12:41:20 +0200

    commit
    Signed-off-by: Andreas Karageorgos <akarageorgos7@protonmail.com>

--- UnityHub-installer.sh
+++ UnityHub-installer.sh
@@ -6,7 +6,7 @@ wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubu
 apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 
-wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 apt update
 apt install unityhub
