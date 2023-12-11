36cf5fe3022b17ba361068b86348f3d6bb5038b5
Andreas Karageorgos <akarageorgos7@protonmail.com>
2023-12-11 14:14:40 +0200

    commit
    Signed-off-by: Andreas Karageorgos <akarageorgos7@protonmail.com>

--- UnityHub-installer.sh
+++ UnityHub-installer.sh
@@ -12,7 +12,7 @@ sudo apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 sudo rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
-sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 sudo apt update
 sudo apt install unityhub -y
 
@@ -20,3 +20,5 @@ clear
 
 echo "UnityHub is installed on your System."
 echo "To remove it type 'sudo apt remove unityhub'"
+
+
