b91c029b3aeb31f6b9a3aadd6d9b48cdcbc8629c
Andreas Karageorgos <akarageorgos7@protonmail.com>
2023-12-27 07:43:15 +0200

    commit
    Signed-off-by: Andreas Karageorgos <akarageorgos7@protonmail.com>

--- UnityHub-installer.sh
+++ UnityHub-installer.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+check_internet() {
+    ping -c 1 google.com > /dev/null 2>&1
+}
+
 if [ "$EUID" -ne 0 ]; then
     echo "This script needs to be run with root privileges. Please enter your password."
     sudo "$0" "$@"  # Re-run the script with sudo
     exit $?
 fi
 
-sudo apt update
-clear
+if ! check_internet; then
+    echo "No internet access. Exiting the script."
+    exit 1
+fi
+
+echo "Updating packages..."
+sudo apt update 1> /dev/null 2> /dev/null
+echo "Done."
+
+
 
 while true; do
     echo "Do you want to also install C# (dotnet-sdk) to your system ? (yes/no)"
@@ -15,8 +27,10 @@ while true; do
 
     case "$answer" in
         yes)
-            sudo apt install apt-transport-https -y
-            sudo apt install dotnet-sdk-7.0 -y
+            echo "Installing C# (dotnet-sdk) to your system."
+            sudo apt install apt-transport-https -y 1> /dev/null 2> /dev/null
+            sudo apt install dotnet-sdk-7.0 -y 1> /dev/null 2> /dev/null
+            echo "Done."
             break
             ;;
         no)
@@ -28,18 +42,17 @@ while true; do
     esac
 done
 
-sudo apt install wget -y
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
-sudo apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
-rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
+echo "Installing UnityHub..."
+sudo apt install wget -y 1> /dev/null 2> /dev/null
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb 1> /dev/null 2> /dev/null
+sudo apt install ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb 1> /dev/null 2> /dev/null
+rm libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb 1> /dev/null 2> /dev/null
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-sudo apt update
-sudo apt install unityhub -y
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list' 1> /dev/null 2> /dev/null
+sudo apt update 1> /dev/null 2> /dev/null
+sudo apt install unityhub -y 1> /dev/null 2> /dev/null
 
-clear
+echo "Done."
 
 echo "UnityHub is installed on your System."
 echo "To remove it type 'sudo apt remove unityhub'"
-
-
